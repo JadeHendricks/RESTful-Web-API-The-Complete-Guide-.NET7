695e84e13ef7375a4e1116cf22abd03c15fe8dc4
Jade <hendricksjade@gmail.com>
2023-05-01 15:14:27 +0200

    add(villaRoute): adding in a villa model and controller and setting up our first endpoint. (Get all Villas)

---
+++ MagicVilla_Villa/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //ControllerBase for API, Controller for views and MVC extra features that we don't need
+    //using [controller] inside of route will automatically use the className - "controller" if you want to do that.
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool View" },
+                new Villa { Id = 2, Name = "Beach View" }
+            };
+        }
+    }
+}
