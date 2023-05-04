85a9e17d2f89ff6ed20ae6ad3cabc94260a27929
Jade <hendricksjade@gmail.com>
2023-05-04 16:58:10 +0200

    add(models): adding in the DTO's and Models into the web application. Adding a utility project for static content, that we want to use in our other projects.

---
+++ MagicVilla_Villa/MagicVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,13 @@
+﻿using static MagicVilla_Utility.SD;
+
+namespace MagicVilla_Web.Models
+{
+    public class APIRequest
+    {
+        //ApiType, we need to get the type of the request first
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string Token { get; set; }
+    }
+}
